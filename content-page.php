1cc98fdf3898001b2e16ee43b7c353248f6d164d
TheCampagnards <konstantin.sidorenko@orange.fr>
2016-01-27 16:50:50 +0100

    commit static

--- content-page.php
+++
@@ -1,6 +0,0 @@
-<article>
-  <h1><?php the_title() ?></h1>
-  <div>
-    <?php the_content() ?>
-  </div>
-</article>
--- maison.php
+++
@@ -1,7 +0,0 @@
-<article>
-	<div class="article-maison-text">
-		<h2><?php echo($post->post_title); ?></h2>
-		<p><?php echo strip_tags($post->post_content); ?></p>
-	</div>
-	<?php preg_match_all('/<img[^>]+>/i',$post->post_content, $result); print_r($result[0][0]);?>
-</article>
