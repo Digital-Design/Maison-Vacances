d2a4c37acfc003be6f3ec77ffece16413778b79f
Konstantin <konstantin.sidorenko@orange.fr>
2016-02-15 16:32:16 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $("a[href^='#']").click(function(){
-	var the_id = $(this).attr("href");
+	var theId = $(this).attr("href");
 	$(this).parent().parent().find("li").each(function() {
 		$(this).removeClass("active");
 	});
 	$(this).parent().addClass("active");
 	$("html, body").animate({
-		scrollTop:$(the_id).offset().top - 50
+		scrollTop:$(theId).offset().top - 50
 	}, "slow");
 	return false;
 });
