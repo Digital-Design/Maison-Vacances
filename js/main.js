37fc541c1cba9866f9d8ca4fd920ce6de7dad349
Konstantin Sidorenko <konstantin@yellowcake.net>
2016-02-15 17:58:13 +0100

    fix scroll google map

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 $("a[href^='#']").click(function(){
 	var theId = $(this).attr("href");
-	$(this).parent().parent().find("li").each(function() {
-		$(this).removeClass("active");
-	});
-	$(this).parent().addClass("active");
-	$("html, body").animate({
-		scrollTop:$(theId).offset().top - 50
-	}, "slow");
-	return false;
-});
-
-$("#calendar").fullCalendar({
-  googleCalendarApiKey: "AIzaSyAzcmUADh2dOxpfUW3XLRcQRmtSJHrA8EE",
-  events: {
-    googleCalendarId: "konstantin@yellowcake.net"
+  var exceptions = ["#carousel-example-generic"];
+  if(jQuery.inArray(theId, exceptions)){
+  	$(this).parent().parent().find("li").each(function() {
+  		$(this).removeClass("active");
+  	});
+  	$(this).parent().addClass("active");
+  	$("html, body").animate({
+  		scrollTop:$(theId).offset().top - 50
+  	}, "slow");
+  	return false;
   }
 });
