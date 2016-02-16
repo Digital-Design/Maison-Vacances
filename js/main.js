f92942d037ef1ef895ae4e3191525aed1fc6b6a4
TheCampagnards <konstantin.sidorenko@orange.fr>
2016-02-16 17:55:39 +0100

    fix la maison image dessous et ajout map résa

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
 $("a[href^='#']").click(function(){
 	var theId = $(this).attr("href");
-  var exceptions = ["#carousel-example-generic"];
-  if(jQuery.inArray(theId, exceptions)){
-  	$(this).parent().parent().find("li").each(function() {
-  		$(this).removeClass("active");
-  	});
-  	$(this).parent().addClass("active");
-  	$("html, body").animate({
-  		scrollTop:$(theId).offset().top - 50
-  	}, "slow");
-  	return false;
-  }
+	var exceptions = ["#carousel-example-generic"];
+	if(jQuery.inArray(theId, exceptions)){
+		$(this).parent().parent().find("li").each(function() {
+			$(this).removeClass("active");
+		});
+		$(this).parent().addClass("active");
+		$("html, body").animate({
+			scrollTop:$(theId).offset().top - 50
+		}, "slow");
+		return false;
+	}
+});
+
+var myLatLng = {lat: 47.9858635, lng: -4.4660625};
+
+// Create a map object and specify the DOM element for display.
+var map = new google.maps.Map(document.getElementById('map'), {
+	center: myLatLng,
+	scrollwheel: false,
+	zoom: 12
+});
+
+// Create a marker and set its position.
+var marker = new google.maps.Marker({
+	map: map,
+	position: myLatLng,
+	title: 'Hello World!'
 });
