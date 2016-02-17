79df5f3b47840c6a5785241e095b37c52ce7ee4a
Konstantin Sidorenko <konstantin@yellowcake.net>
2016-02-17 17:27:31 +0100

    fix nav

--- js/main.js
+++ js/main.js
@@ -2,20 +2,20 @@ $(document).ready(function () {
 	$(document).on("scroll", onScroll);
 
 	//permet le scroll swing
-	$('a[href^="#"]').on('click', function (e) {
+	$("a[href^='#']").on("click", function (e) {
 		e.preventDefault();
 		$(document).off("scroll");
 
-		$('a').parent().each(function () {
-			$(this).removeClass('active');
+		$("a").parent().each(function () {
+			$(this).removeClass("active");
 		});
-		$(this).parent().addClass('active');
+		$(this).parent().addClass("active");
 
 		var target = this.hash;
 		$target = $(target);
-		$('html, body').stop().animate({
-			'scrollTop': $target.offset().top - $('.navbar').height()
-		}, 500, 'swing', function () {
+		$("html, body").stop().animate({
+			"scrollTop": $target.offset().top - $(".navbar").height()
+		}, 500, "swing", function () {
 			$(document).on("scroll", onScroll);
 		});
 	});
@@ -24,12 +24,12 @@ $(document).ready(function () {
 //permet d'ajouter activer sur certain lien
 function onScroll(event){
 	var scrollPos = $(document).scrollTop();
-	$('.nav-link a').each(function () {
+	$(".nav-link a").each(function () {
 		var currLink = $(this);
 		var refElement = $(currLink.attr("href"));
 		if(refElement.position() !== undefined)
-			if (refElement.position().top <= scrollPos + $('.navbar').height() && refElement.position().top + refElement.height() > scrollPos + $('.navbar').height() - 40) {
-				$('.nav-link').removeClass("active");
+			if (refElement.position().top <= scrollPos + $(".navbar").height() && refElement.position().top + refElement.height() > scrollPos + $(".navbar").height() - 45) {
+				$(".nav-link").removeClass("active");
 				currLink.parent().addClass("active");
 			}
 			else{
@@ -40,7 +40,7 @@ function onScroll(event){
 
 //google map
 var myLatLng = {lat: 47.9858635, lng: -4.4660625};
-var map = new google.maps.Map(document.getElementById('map'),{
+var map = new google.maps.Map(document.getElementById("map"),{
 	center: myLatLng,
 	scrollwheel: false,
 	zoom: 12
@@ -49,5 +49,5 @@ var map = new google.maps.Map(document.getElementById('map'),{
 var marker = new google.maps.Marker({
 	map: map,
 	position: myLatLng,
-	title: 'Maison de Vacances'
+	title: "Maison de Vacances"
 });
