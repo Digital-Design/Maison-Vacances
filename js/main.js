784fe8b34710e86622cafd2cfaeb2b85add4802e
Konstantin Sidorenko <konstantin@yellowcake.net>
2016-02-17 17:41:03 +0100

    fix css codacy

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
+//permet d'ajouter activer sur certain lien
+function onScroll(){
+	var scrollPos = $(document).scrollTop();
+	$(".nav-link a").each(function () {
+		var currLink = $(this);
+		var refElement = $(currLink.attr("href"));
+		if(refElement.position() !== undefined)
+			if (refElement.position().top <= scrollPos + $(".navbar").height() && refElement.position().top + refElement.height() > scrollPos + $(".navbar").height() - 40) {
+				$(".nav-link").removeClass("active");
+				currLink.parent().addClass("active");
+			}
+			else{
+				currLink.parent().removeClass("active");
+			}
+	});
+}
+onScroll();
+
 $(document).ready(function () {
 	$(document).on("scroll", onScroll);
-
 	//permet le scroll swing
 	$("a[href^='#']").on("click", function (e) {
 		e.preventDefault();
@@ -11,32 +28,14 @@ $(document).ready(function () {
 		});
 		$(this).parent().addClass("active");
 
-		var target = this.hash;
-		$target = $(target);
+		var $target = $(this.hash);
 		$("html, body").stop().animate({
-			"scrollTop": $target.offset().top - $(".navbar").height()
+			"scrollTop": $target.offset().top - $(".navbar").height() + 2
 		}, 500, "swing", function () {
 			$(document).on("scroll", onScroll);
 		});
 	});
 });
-
-//permet d'ajouter activer sur certain lien
-function onScroll(event){
-	var scrollPos = $(document).scrollTop();
-	$(".nav-link a").each(function () {
-		var currLink = $(this);
-		var refElement = $(currLink.attr("href"));
-		if(refElement.position() !== undefined)
-			if (refElement.position().top <= scrollPos + $(".navbar").height() && refElement.position().top + refElement.height() > scrollPos + $(".navbar").height() - 45) {
-				$(".nav-link").removeClass("active");
-				currLink.parent().addClass("active");
-			}
-			else{
-				currLink.parent().removeClass("active");
-			}
-	});
-}
 
 //google map
 var myLatLng = {lat: 47.9858635, lng: -4.4660625};
@@ -46,7 +45,7 @@ var map = new google.maps.Map(document.getElementById("map"),{
 	zoom: 12
 });
 //ajout du markeur
-var marker = new google.maps.Marker({
+new google.maps.Marker({
 	map: map,
 	position: myLatLng,
 	title: "Maison de Vacances"
